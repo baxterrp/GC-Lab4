81e80f75ac8dbe127baf55817c3d8f1e19ca0ec4
Robert <baxterrp@gmail.com>
2016-06-10 07:58:53 -0400

    Added comments

--- src/CalculateFactorials.java
+++ src/CalculateFactorials.java
@@ -12,19 +12,25 @@ public class CalculateFactorials {
 			System.out
 					.print("Enter an integer that's greater than 0 but less than 10: ");
 			number = scan.nextInt();
+
+			// ensure number is between 0 and 10 with if
 			if (number > 0 && number < 10) {
 				long factorial = 1;
 
+				// loop from number down to 1 multiplying factorial by each
+				// iteration of i
 				for (int i = number; i > 0; i--) {
 					factorial *= i;
 				}
 
+				// output factorial
 				System.out.println("The factorial of " + number + " is "
 						+ factorial);
 				System.out.println("Continue?(y/n)");
 				scan.nextLine();
 				option = scan.nextLine();
 			} else {
+				// if number is outside of valid range - continue loop
 				System.out.println("Invalid number");
 				continue;
 			}
